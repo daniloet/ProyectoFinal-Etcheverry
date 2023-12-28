af5313d4653ea5a01e1b9abdb2cda6f899c023c1
Danilo Etcheverry <lodanii70@gmail.com>
2023-12-28 19:53:02 -0300

    proyecto

---
+++ script.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const productos = [
+        { id: 1, nombre: 'Pelota', precio: 20.00 },
+        { id: 2, nombre: 'Botines', precio: 50.00 },
+        { id: 3, nombre: 'Camiseta', precio: 30.00 },
+        { id: 4, nombre: 'Medias', precio: 10.00 }
+    ];
+
+    const listaProductos = document.getElementById('lista-productos');
+
+    productos.forEach(producto => {
+        const li = document.createElement('li');
+        li.innerHTML = `${producto.nombre} - $${producto.precio.toFixed(2)} <button onclick="addToCart(${producto.id})">Agregar al carrito</button>`;
+        listaProductos.appendChild(li);
+    });
+    window.addToCart = function (productoId) {
+        const productoSeleccionado = productos.find(producto => producto.id === productoId);
+
+        const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+        const itemsCarrito = document.getElementById('items-carrito');
+        const totalCarrito = document.getElementById('total-carrito');
+
+        carrito.push(productoSeleccionado);
+
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+        updateCartDOM(itemsCarrito, totalCarrito);
+    };
+
+    function updateCartDOM(itemsCarrito, totalCarrito) {
+        const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+        itemsCarrito.innerHTML = '';
+
+        let total = 0;
+
+        carrito.forEach(producto => {
+            const li = document.createElement('li');
+            li.innerHTML = `${producto.nombre} - $${producto.precio.toFixed(2)}`;
+            itemsCarrito.appendChild(li);
+
+            total += producto.precio;
+        });
+
+        totalCarrito.textContent = total.toFixed(2);
+    }
+
+    updateCartDOM(document.getElementById('items-carrito'), document.getElementById('total-carrito'));
+});
